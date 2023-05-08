e6733ee732db95dea2716fa3dc64ebcc7aadbf15
Cesar Li Chen <cesarli619@gmail.com>
2023-05-08 17:28:26 -0400

    Initial commit of factorial and euclids

---
+++ euclidsGDC.js
@@ -0,0 +1,5 @@
+// Euclid's algorithm for Greatest Common Divisonr (GCD)
+
+function euclids(num1, num2) {
+
+}
---
+++ factorial.js
@@ -0,0 +1,11 @@
+// Factorial exercise
+
+function factorial(num) {
+    let total = 1;
+    
+    for (let i = 1; i <= num; i++) {
+      total = total * i;
+    }
+    
+    return total;
+  }
