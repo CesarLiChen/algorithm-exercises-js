422e2d7146470f60f0e7786d13f86ba6505d5f94
Cesar Li Chen <cesarli619@gmail.com>
2023-05-09 15:23:35 -0400

    Completes 2nd version of Euclids

--- euclidsGCD2.js
+++ euclidsGCD2.js
@@ -1,3 +1,23 @@
 // Euclid's algorithm for Greatest Common Divisor (GCD).
 // More efficient version, with remainder.
+// Following this:
+// https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclidean_algorithm
+// implementation.
 
+function euclids2(n1, n2) {
+    // makes sure n1 is biggest.
+    if (n2 > n1) {
+        let tmp = n1;
+        n1 = n2;
+        n2 = tmp;
+    }
+    
+    while (n2 != 0) {
+        
+        let tmp = n1;
+        n1 = n2;
+        n2 = tmp % n2
+    }
+    
+    return n1;
+}
