0bb92d0373a1cc2f30c2dda97d4ee022c640bfea
Cesar Li Chen <cesarli619@gmail.com>
2023-05-12 04:23:02 -0400

    Initial commit for exercise 'truncate'

---
+++ truncate.js
@@ -0,0 +1,13 @@
+/*
+Write a function that truncates a string (first argument) 
+if it is longer than the given maximum string length (second argument) 
+as a given input. It should return a single, truncated string. 
+However, be sure that the last 3 characters of the returned 
+string are an ellipsis (...).
+*/ 
+
+// E.g. truncate("This is a string", 8); -> "This ..."
+
+function truncate(str, num) {
+    
+}
