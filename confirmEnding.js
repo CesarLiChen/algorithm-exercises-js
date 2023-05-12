80391a3cc94f1bd9d0d3e89e1c86c1e01cf4a31c
Cesar Li Chen <cesarli619@gmail.com>
2023-05-12 03:34:47 -0400

    Fixes first solution

--- confirmEnding.js
+++ confirmEnding.js
@@ -9,5 +9,6 @@ function confirmEnding(str, target) {
     // First solution. Incorrect, turns out 'target' can be a word too.
     // return str.charAt(str.length - 1) === target;
 
-    return str.endsWith(target); // Even easier solution
+    return str.substring(str.length - target.length) === target;
+    // return str.endsWith(target); // Even easier solution
 }
