5e6a2880fe8c2b7d2613cfd472bd673543b6e941
Cesar Li Chen <cesarli619@gmail.com>
2023-05-11 20:29:16 -0400

    Initial commit of version 2's largestOfThree

---
+++ largestOfThree2.js
@@ -0,0 +1,10 @@
+// Given an array of arrays as input, 
+// return a new array that contains the biggest 
+// number from each subarray.
+
+// This version will try and use JS's .map, and .reduce
+// methods for arrays.
+
+function largestOfThree2(arr) {
+    
+}
